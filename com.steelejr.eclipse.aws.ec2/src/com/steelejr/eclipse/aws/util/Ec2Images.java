f4e77a43951b431b988ff4fee6b4773cd112b053
John Steele <programjsteele@gmail.com>
2011-11-14 18:13:12 -0800

    Updated the Ec2 images to use the wst image set.

---
+++ com.steelejr.eclipse.aws.ec2/src/com/steelejr/eclipse/aws/util/Ec2Images.java
@@ -0,0 +1,55 @@
+package com.steelejr.eclipse.aws.util;
+
+
+/**
+ * A registry for common images used in the Ec2 plug-in.
+ */
+public interface Ec2Images {
+	
+	/** Identifies the location for the new instance image. */
+	public static final String IMG_NEW_EC2_INSTANCE_LOC = "/icons/new_ec2_instance.gif";
+	/** Identifies the key for the new instance image. */
+	public static final String IMG_NEW_EC2_INSTANCE = "new_ec2_instance";
+	
+	
+	/** Identifies the location for the refresh image. */
+	public static final String IMG_REFRESH_LOC = "/icons/refresh.gif";
+	/** Identifies the key for the refresh image. */
+	public static final String IMG_REFRSH = "refresh";
+	
+	
+	/** Identifies the location for the update image. */
+	public static final String IMG_UPDATE = "update";
+	/** Identifies the key for the update image. */
+	public static final String IMG_UPDATE_LOC = "/icons/update.gif";
+	
+	
+	/** Identifies the location for the run image. */
+	public static final String IMG_LAUNCH_RUN = "run";
+	/** Identifies the key for the run image. */
+	public static final String IMG_LAUNCH_RUN_LOC = "/icons/launch_run.gif";
+	
+	
+	/** Identifies the location for the stop image. */
+	public static final String IMG_LAUNCH_STOP = "stop";
+	/** Identifies the key for the stop image. */
+	public static final String IMG_LAUNCH_STOP_LOC = "/icons/launch_stop.gif";
+	
+	
+	/** Identifies the location for the instance started image.  */
+	public static final String IMG_INSTANCE_STARTED = "instance_started";
+	/** Identifies the key for the instance started image. */
+	public static final String IMG_INSTANCE_STARTED_LOC = "/icons/instance_state/state_instance_started.gif";
+	
+	
+	/** Identifies the location for the instance stopped image. */
+	public static final String IMG_INSTANCE_STOPPED = "instance_stopped";
+	/** Identifies the key for the instance stopped image. */
+	public static final String IMG_INSTANCE_STOPPED_LOC = "/icons/instance_state/state_instance_stopped.gif";
+	
+	
+	/** Identifies the location for the instance pending image. */
+	public static final String IMG_INSTANCE_PENDING = "instance_pending";
+	/** Identifies the key for the instance pending image. */
+	public static final String IMG_INSTSNCE_PENDING_LOC = "/icons/instance_state/state_instance_pending.gif";
+}
